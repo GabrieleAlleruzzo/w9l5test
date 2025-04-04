f92244e9be86d83a2a308608c7bb0e84b1ffc3d7
Gabriele Alleruzzo <alleruzzo.gabriele.96@gmail.com>
2025-04-04 17:02:44 +0200

    Edits footer

--- src/assets/CustomComponents/CustomFooter.jsx
+++ src/assets/CustomComponents/CustomFooter.jsx
@@ -2,16 +2,38 @@ import { Col, Row, Container, Navbar } from "react-bootstrap";
 
 function CustomFooter() {
   return (
-    <Container sticky="bottom" className="bg-black py-5">
+    <Container
+      sticky="bottom"
+      className="bg-black py-5"
+      style={{ color: "grey" }}
+    >
       <Row>
-        <Col>
+        <Col xs={12} md={6} lg={3}>
+          <Col className="d-flex flex-row justify-content-evenly text-start">
+            <i className="bi bi-twitter"></i>
+            <i className="bi bi-facebook"></i>
+            <i className="bi bi-instagram"></i>
+            <i className="bi bi-youtube"></i>
+          </Col>
           <img
             style={{ width: "100px" }}
             src="public\netflix_logo.png"
             alt=""
           />
+
+          <p className=" text-start">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            veritatis corporis ipsam, doloremque alias mollitia, nobis amet
+            porro unde hic dolorem doloribus maiores provident, modi tempore.
+            Optio maxime veritatis minus.
+          </p>
         </Col>
-        <Col className="text-white d-flex flex-column align-items-start">
+        <Col
+          xs={12}
+          md={6}
+          lg={3}
+          className=" d-flex flex-column align-items-start"
+        >
           <h4>Titolo</h4>
           <p className=" text-start">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
@@ -20,7 +42,12 @@ function CustomFooter() {
             Optio maxime veritatis minus.
           </p>
         </Col>
-        <Col className="text-white d-flex flex-column align-items-start">
+        <Col
+          xs={12}
+          md={6}
+          lg={3}
+          className=" d-flex flex-column align-items-start"
+        >
           <h4>Titolo</h4>
           <p className=" text-start">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
@@ -29,7 +56,12 @@ function CustomFooter() {
             Optio maxime veritatis minus.
           </p>
         </Col>
-        <Col className="text-white d-flex flex-column align-items-start ">
+        <Col
+          xs={12}
+          md={6}
+          lg={3}
+          className=" d-flex flex-column align-items-start "
+        >
           <h4>Titolo</h4>
           <p className=" text-start">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
